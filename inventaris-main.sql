3954c08e08f3d92c850533d98d12e465623926ca
ridho-code <60309031+ridho-code@users.noreply.github.com>
2024-06-11 22:35:22 +0800

    database updated

--- inventaris-main.sql
+++ inventaris-main.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jun 04, 2024 at 06:22 PM
+-- Generation Time: Jun 11, 2024 at 02:35 PM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -20,6 +20,26 @@ SET time_zone = "+00:00";
 --
 -- Database: `inventaris-main`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id_admin` int NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `nama` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id_admin`, `username`, `password`, `nama`) VALUES
+(1, 'admin', 'admin', 'admin 1');
 
 -- --------------------------------------------------------
 
@@ -182,27 +202,46 @@ INSERT INTO `tb_pinjam` (`id_pinjam`, `nm_pinjam`, `jml_pinjam`, `surat`, `foto`
 
 CREATE TABLE `tb_user` (
   `id_user` int NOT NULL,
-  `nm_user` varchar(255) NOT NULL,
-  `alamat_user` varchar(255) NOT NULL,
-  `DMS_user` varchar(255) NOT NULL,
-  `jabatan_user` varchar(255) NOT NULL,
-  `hp_user` varchar(255) NOT NULL,
+  `nama` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `alamat` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `dms` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `hp` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `theme` text NOT NULL
+  `foto` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `code` text NOT NULL,
+  `is_verify` int DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `tb_user`
 --
 
-INSERT INTO `tb_user` (`id_user`, `nm_user`, `alamat_user`, `DMS_user`, `jabatan_user`, `hp_user`, `username`, `password`, `theme`) VALUES
-(1, 'Joko', 'BTP', 'DMS.XIV.111.211991', 'KaDept. Keagamaan', '092340932940', 'user', 'user', 'dracula'),
-(2, 'Mark', 'Sudiang', 'DMS.XVI.999.211299', 'Koord. Kerohanian', '08129040292', 'mark', '123', 'dracula');
+INSERT INTO `tb_user` (`id_user`, `nama`, `alamat`, `dms`, `email`, `hp`, `username`, `password`, `foto`, `code`, `is_verify`) VALUES
+(1, 'Joko', 'BTP', 'DMS.XIV.111.211991', '', '092340932940', 'user', 'user', 'dracula', '', 1),
+(2, 'Mark', 'Sudiang', 'DMS.XVI.999.211299', '', '08129040292', 'mark', '123', 'dracula', '', NULL),
+(3, '5', '5', '5', '', '55', '5', '', '1718113575.jpg', '', NULL),
+(4, '5', '5', '5', '', '55', '5', '', '1718113604.jpg', '05acfe17f1fd2d0d3cee1214d938684f', NULL),
+(5, '5', '5', '5', '', '55', '5', '', '1718113623.jpg', '35f7f350782c142d47ca03844b746692', NULL),
+(6, '5', '5', '5', '', '55', '5', '5', '1718113653.jpg', '35f7f350782c142d47ca03844b746692', NULL),
+(7, '5', '5', '5', '', '55', '5', '5', '1718113682.jpg', '87cf161d26c52057da3fa99e165a881d', NULL),
+(8, '6', '6', '66', '', '66', '6', '6', '1718114898.jpg', '8c423036b31209eedcb450ee7d1a65aa', NULL),
+(9, '6', '6', '66', '', '66', '6', '6', '1718114946.jpg', '8fc8b9382f991bb2784a74ad7ab225a9', NULL),
+(10, '6', '6', '66', '', '66', '6', '6', '1718115034.jpg', '7040d9f37edab90d666d42d24af02137', NULL),
+(11, '6', '6', '66', '', '66', '6', '6', '1718115055.jpg', '7040d9f37edab90d666d42d24af02137', NULL),
+(12, 'Ridho', 'BTP', 'DMS.614', 'saputra22022@gmail.com', '081231231', 'ridho', 'ridho', '1718115467.jpg', 'b41eeaa54fa75e3b566d007c142017eb', 1),
+(13, 'Dimas', 'Gowa', 'Tidak Ada', 'saputra22022@gmail.com', '0823123123212', 'dimas', 'dimas', '1718116298.jpg', 'daad4772dd8454d31da98fea7ae9764d', 1);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id_admin`);
 
 --
 -- Indexes for table `tb_anggota`
@@ -245,6 +284,12 @@ ALTER TABLE `tb_user`
 --
 
 --
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id_admin` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `tb_anggota`
 --
 ALTER TABLE `tb_anggota`
@@ -278,7 +323,7 @@ ALTER TABLE `tb_pinjam`
 -- AUTO_INCREMENT for table `tb_user`
 --
 ALTER TABLE `tb_user`
-  MODIFY `id_user` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_user` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
